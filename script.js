71ce7b14714d1edba13ecedd87df594eccc407bb
MindaAndreea <165321457+MindaAndreea@users.noreply.github.com>
2024-06-06 05:46:33 +0300

    JS file updated

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const buton = document.getElementById("joke-btn");
 
-const container = document.querySelector(".joke-container");
+const jokeContainer = document.querySelector(".joke-container");
+
+async function addJoke() {
+  let joke = "";
+  jokeContainer.textContent = "Loading...";
+
+  const data = await getDataFromAPI();
+  if (data.type === "single") {
+    joke = data.joke;
+  } else {
+    joke = data.setup + " " + data.delivery;
+  }
+  jokeContainer.textContent = joke;
+}
 
 async function getDataFromAPI() {
   const url =
-    " https://v2.jokeapi.dev/joke/Programming,Dark,Spooky?blacklistFlags=sexist ";
+    " https://v2.jokeapi.dev/joke/Programming,Dark,Spooky?blacklistFlags=racist,sexist ";
 
   try {
     const response = await fetch(url);
-    console.log(response);
+    // console.log(response);
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
+    return data;
   } catch (error) {
     console.log("Eroare API: " + error);
   }
 }
 
-getDataFromAPI();
+buton.addEventListener("click", addJoke);
