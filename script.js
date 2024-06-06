45dcb924fad7607331480910ff786eb1542dca05
MindaAndreea <165321457+MindaAndreea@users.noreply.github.com>
2024-06-06 06:59:00 +0300

    Adding the property to tell the jokes out loud as well.

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ async function addJoke() {
     joke = data.setup + " " + data.delivery;
   }
   jokeContainer.textContent = joke;
+
+  tellTheJoke();
 }
 
 async function getDataFromAPI() {
@@ -31,3 +33,16 @@ async function getDataFromAPI() {
 }
 
 buton.addEventListener("click", addJoke);
+
+function tellTheJoke() {
+  VoiceRSS.speech({
+    key: "5ff4b1b6ab8143f488a977bc3a2f2a9c",
+    src: "Hello, world!",
+    hl: "en-us",
+    v: "Linda",
+    r: 1,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+}
